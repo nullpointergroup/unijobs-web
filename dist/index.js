69d620225dde452334592c5a6e5b46f02a50bc4b
franexposito <8macau8@gmail.com>
2016-09-21 20:05:28 +0200

    server config

--- dist/index.js
+++ dist/index.js
@@ -2,10 +2,19 @@ var express = require('express');
 var http = require('http');
 var logger = require('morgan');
 var app = express();
+
 app.use(logger());
+
+app.configure(function(){
+  app.use(express.static(path.join(__dirname, '/')));
+});
 
 app.get('/', function (req, res) {
   res.render('index.html');
+});
+
+app.get('/hello', function (req, res) {
+  res.send('Hellow');
 });
 
 var server = http.createServer(app);
