447ca1a2ae0fbd2ff3e6dc95616151a18053eea5
franexposito <8macau8@gmail.com>
2016-09-21 19:50:53 +0200

    routes

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,9 @@ var logger = require('morgan');
 var app = express();
 app.use(logger());
 
+app.get('/', function (req, res) {
+  res.render('index.html');
+});
 
 var server = http.createServer(app);
 server.listen(process.env.PORT || 5000);
